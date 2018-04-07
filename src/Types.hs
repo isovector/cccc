cd08fc5e237a71672a9c0c8c0c4a7b819729d902
Sandy Maguire <sandy@sandymaguire.me>
2018-04-07 17:29:06 +0300

    newtype around Subst and normalize them

--- src/Types.hs
+++ src/Types.hs
@@ -5,11 +5,12 @@
 {-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE OverloadedLists            #-}
+{-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE PatternSynonyms            #-}
 {-# LANGUAGE StandaloneDeriving         #-}
 {-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TupleSections              #-}
 {-# LANGUAGE TypeApplications           #-}
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE UndecidableInstances       #-}
@@ -17,6 +18,7 @@
 
 module Types where
 
+import           Control.Lens ((<&>))
 import           Bound hiding (instantiate)
 import           Bound.Scope hiding (instantiate)
 import           Control.Arrow (first, second)
@@ -151,25 +153,24 @@ deriving instance Show a => Show (Exp a)
 instantiate :: Scheme -> TI Type
 instantiate (Scheme vars t) = do
   nvars <- traverse (const newTyVar) vars
-  pure $ apply (M.fromList (zip vars nvars)) t
+  pure $ apply (Subst $ M.fromList (zip vars nvars)) t
 
 unify :: Type -> Type -> TI Subst
 unify (TArr l r) (TArr l' r') = do
   s1 <- unify l l'
   s2 <- unify (apply s1 r) (apply s1 r')
-  pure $ composeSubst s1 s2
+  pure $ s1 <> s2
 unify (TProd l r) (TProd l' r') = do
   s1 <- unify l l'
   s2 <- unify (apply s1 r) (apply s1 r')
-  pure $ composeSubst s1 s2
+  pure $ s1 <> s2
 unify (TSum l r) (TSum l' r') = do
   s1 <- unify l l'
   s2 <- unify (apply s1 r) (apply s1 r')
-  pure $ composeSubst s1 s2
+  pure $ s1 <> s2
 unify (TVar u) t  = varBind u t
 unify t (TVar u)  = varBind u t
 unify TInt TInt   = pure mempty
-unify TBool TBool = pure mempty
 unify t1 t2       = throwE $
   mconcat
     [ "types don't unify: '"
@@ -180,24 +181,26 @@ unify t1 t2       = throwE $
     ]
 
 varBind :: TName -> Type -> TI Subst
-varBind u t | t == TVar u
-              = pure mempty
-            | S.member u (free t)
-              = throwE
-                $ mconcat
-                  [ "occurs check: '"
-                  , show u
-                  , "' vs '"
-                  , show t
-                  , "'"
-                  ]
-            | otherwise = pure [(u, t)]
+varBind u t
+  | t == TVar u
+    = pure mempty
+  | S.member u (free t)
+    = throwE
+      $ mconcat
+        [ "occurs check: '"
+        , show u
+        , "' vs '"
+        , show t
+        , "'"
+        ]
+  | otherwise
+    = pure $ Subst [(u, t)]
 
 infer :: (Show a, Ord a) => (Int -> a) -> TypeEnv a -> Exp a -> TI (Subst, Type)
 infer f env (Assert e t) = do
   (s1, t1) <- infer f env e
   s2 <- unify t t1
-  pure (composeSubst s2 s1, t)
+  pure (s1 <> s2, t)
 infer _ (TypeEnv env) (V a) =
   case M.lookup a env of
     Nothing -> throwE $ "unbound variable: '" <> show a <> "'"
@@ -210,7 +213,7 @@ infer f env (Let bs b) = do
   let t' = generalize (apply s1 env) t1
       env' = TypeEnv $ M.insert name t' $ unTypeEnv env
   (s2, t2) <- infer f (apply s1 env') e2
-  pure (composeSubst s1 s2, t2)
+  pure (s1 <> s2, t2)
 infer _ _ (Lit (LInt _))  = pure (mempty, TInt)
 infer _ _ (Lit (LBool _)) = pure (mempty, TBool)
 infer f (TypeEnv env) (Lam x) = do
@@ -227,7 +230,7 @@ infer f env exp@(e1 :@ e2) =
     (s1, t1) <- infer f env e1
     (s2, t2) <- infer f (apply s1 env) e2
     s3 <- unify (apply s2 t1) (TArr t2 tv)
-    pure (composeSubst s3 $ composeSubst s2 s1, apply s3 tv)
+    pure (s1 <> s2 <> s3, apply s3 tv)
   `catchE` \e -> throwE $
     mconcat
       [ e
@@ -251,7 +254,17 @@ pattern TBool = TSum TUnit TUnit
 
 class Types a where
   free :: a -> Set TName
-  apply :: Map TName Type -> a -> a
+  apply :: Subst -> a -> a
+
+
+flatten :: Subst -> Subst
+flatten (Subst x) = fix $ \(Subst final) ->
+  Subst $ M.fromList $ M.assocs x <&> \(a, b) -> (a,) $
+    case b of
+      TVar n -> final M.! n
+      z -> z
+
+
 
 instance Types Type where
   free (TVar a)    = [a]
@@ -263,7 +276,7 @@ instance Types Type where
   free (TProd a b) = free a <> free b
   free (TSum a b)  = free a <> free b
 
-  apply s (TVar n)    = maybe (TVar n) id $ M.lookup n s
+  apply s (TVar n)    = maybe (TVar n) id $ M.lookup n $ unSubst s
   apply s (TArr a b)  = TArr (apply s a) (apply s b)
   apply s (TProd a b) = TProd (apply s a) (apply s b)
   apply s (TSum a b)  = TSum (apply s a) (apply s b)
@@ -277,12 +290,16 @@ instance Types Scheme where
 
   -- apply all `s` that are not quantified?
   apply s (Scheme vars t) =
-    Scheme vars $ apply (foldr M.delete s vars) t
+    Scheme vars $ apply (Subst $ foldr M.delete (unSubst s) vars) t
 
-type Subst = Map TName Type
+newtype Subst = Subst
+  { unSubst :: Map TName Type }
+  deriving (Eq, Show)
 
-composeSubst :: Subst -> Subst -> Subst
-composeSubst s1 s2 = fmap (apply s1) s2 <> s1
+instance Monoid Subst where
+  mempty = Subst mempty
+  mappend s1 (Subst s2) =
+    Subst $ fmap (apply s1) s2 <> unSubst s1
 
 newtype TypeEnv a = TypeEnv
   { unTypeEnv :: Map a Scheme
